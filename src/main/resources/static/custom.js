c8d3cc3268d74306732e6b0109e73df9d6234f58
Jack Werner <jwerner@Jacks-MacBook-Pro-2.local>
2020-11-09 17:17:04 -0500

    updated style sheets and javascript page

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,8 +1,34 @@
 let map;
+// let coords = {lat: 39.9612, lng: -82.9988};
+
+var contentString = '<h2>' + city + ', ' + state + '</h2>';
 
 function initMap() {
+
+    let infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    let image = {
+        url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png", 
+    scaledSize: new google.maps.Size(30, 30)};
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.9612, lng: -82.9988},
-    zoom: 10
+    center: coords,
+    zoom: 10,
+    scrollwheel: false, 
   });
+
+    let marker = new google.maps.Marker({
+    position: coords,    
+    map: map,  
+    icon: image,
+    animation: google.maps.Animation.DROP,
+    });
+
+    google.maps.event.addListener(marker, 'click', function(){
+        infoWindow.open(map,marker);
+    });
+  
 }
+
